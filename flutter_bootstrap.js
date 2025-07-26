a34caf3aa7fd9e4db3ad439fd362156e3cf30e7e
Vadim 1 <dorozkavgovne@gmail.com>
2025-07-26 22:19:09 +0500

    Fix base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3597524576"
+    serviceWorkerVersion: "3225117699"
   }
 });
